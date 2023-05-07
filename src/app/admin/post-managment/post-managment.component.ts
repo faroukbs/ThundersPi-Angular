a9811d0cfed324da24b5854e890956aa2950bf81
faroukbs <farouk.boussaid@esprit.tn>
2023-05-07 14:46:45 +0100

    last merge

--- src/app/admin/post-managment/post-managment.component.ts
+++ src/app/admin/post-managment/post-managment.component.ts
@@ -9,40 +9,38 @@ import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-post-managment',
   templateUrl: './post-managment.component.html',
-  styleUrls: ['./post-managment.component.css',
-  '../../../assets/front/css/style.css',
-  '../../../assets/front/css/slick.css',
-  '../../../assets/front/css/responsive.css',
-  '../../../assets/front/css/nice-select.css',
-  '../../../assets/front/css/magnific-popup.css',
-  '../../../assets/front/css/jquery.nice-number.min.css',
-  '../../../assets/front/css/font-awesome.min.css',
-  '../../../assets/front/css/default.css',
-  '../../../assets/front/css/bootstrap.min.css',
-  '../../../assets/front/css/animate.css']
+  styleUrls: [
+    './post-managment.component.css',
+    '../../../assets/front/css/style.css',
+    '../../../assets/front/css/slick.css',
+    '../../../assets/front/css/responsive.css',
+    '../../../assets/front/css/nice-select.css',
+    '../../../assets/front/css/magnific-popup.css',
+    '../../../assets/front/css/jquery.nice-number.min.css',
+    '../../../assets/front/css/font-awesome.min.css',
+    '../../../assets/front/css/default.css',
+    '../../../assets/front/css/bootstrap.min.css',
+    '../../../assets/front/css/animate.css',
+  ],
 })
 export class PostManagmentComponent implements OnInit {
-  listofpost:Post[];
-  constructor(private ps:PostServiceService) { }
+  listofpost: Post[];
+  constructor(private ps: PostServiceService) {}
 
   ngOnInit(): void {
     this.getallpost();
   }
-getallpost(){
-  this.ps.getPost().subscribe(
-    data=>{
-      console.log(data)
-      this.listofpost=data;
-    }
-  )
-}
-supprimer(post :any){
-  this.ps.deletePost(post.postId).subscribe(()=>this.ps.getPost().subscribe(
-    data=>{
-      this.listofpost=data
-      
-    }
-  )
-  );
-}
+  getallpost() {
+    this.ps.getPost().subscribe((data) => {
+      console.log(data);
+      this.listofpost = data;
+    });
+  }
+  supprimer(post: any) {
+    this.ps.deletePost(post.postId).subscribe(() =>
+      this.ps.getPost().subscribe((data) => {
+        this.listofpost = data;
+      })
+    );
+  }
 }
